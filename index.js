0e61f5d57f47fdd2d5518c1883d058f3ef55b5ec
kimberly <maldakimberly228@gmail.com>
2025-03-06 14:04:40 +0300

    create a file

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+// function multiply() {
+//    const num1 = 31;
+//    const num2 = 2;
+//     const multiply = num1 * num2;
+    
+// }
+// const num1 = 31;
+// const num2 = 2;
 
-// Write your code here
+// function multiply(num1, num2){
+//     return num1*num2;
+// };
+const num1=31;
+const num2=2;
+const multiply =num1*num2
+
+console.log(multiply);
+const random=Math.floor(Math.random()*10)+1;
+const num3 = 20;
+const num4 =5 ;
+const mod = num3 / num4;
+console.log('remainder 4');
+
+const max = Math.max(1, 2, 3, 4, 5,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+console.log(20);
